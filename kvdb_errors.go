645037c4fe6c738b688c1c54328a7491354e9085
Abhishek Bagchi <me@abhishekbagchi.co.uk>
2020-04-04 20:52:00 +0100

    go fmt on kvdb_errors

--- kvdb_errors.go
+++ kvdb_errors.go
@@ -3,10 +3,10 @@ package kvdb
 type ErrorType uint8
 
 const (
-	DatabaseNameOutOfBounds  ErrorType = iota + 1
-	DatabaseStateInvalid     ErrorType = iota + 1
-	DatabaseKeyExists        ErrorType = iota + 1
-	DatabaseKeyNotPresent    ErrorType = iota + 1
+	DatabaseNameOutOfBounds ErrorType = iota + 1
+	DatabaseStateInvalid    ErrorType = iota + 1
+	DatabaseKeyExists       ErrorType = iota + 1
+	DatabaseKeyNotPresent   ErrorType = iota + 1
 )
 
 func (err *ErrorType) Error() string {
